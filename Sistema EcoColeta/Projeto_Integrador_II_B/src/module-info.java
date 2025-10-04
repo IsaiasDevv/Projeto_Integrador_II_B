94a40c776fc79c6ed22ae1e0cdc8c26acb49246d
IsaiasDevv <isaias.devs@gmail.com>
2025-10-03 22:49:12 -0300

    Removendo Pasta Duplicada

--- Sistema EcoColeta/Projeto_Integrador_II_B/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Projeto_Integrador_II_B {
-    requires java.desktop;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.fxml;
-
-    opens ecoColeta to javafx.graphics, javafx.fxml;
-}
